868f5697c02b8faa5188145f67066be47453fdbd
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2026-05-01 01:42:05 +0600

    cc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './users/users.module';
+import { CompanyModule } from './company/company.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, CompanyModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
