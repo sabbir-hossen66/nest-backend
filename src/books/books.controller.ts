a5fa0d496c2c92770bc38c231a4dbe7e9bb9268d
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2026-05-01 23:49:32 +0600

    cc

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+    @Get()
+    getBooks() {
+        return `hello books`
+    }
+}
